54bb628814f6e8c9e2d312eedd7373b0beb23290
Temerold <67090612+Temerold@users.noreply.github.com>
2022-06-02 21:04:28 +0200

    [Linguistics] Correct formatting

--- compile_main_c.bat
+++ compile_main_c.bat
@@ -1,2 +1,2 @@
-:: ! IMPORTANT: Remember to run this script when `main.c` is altered to compile it.
+:: ! IMPORTANT: Remember to run this script when `main.c` is altered to compile it
 gcc main.c -o count
