d6675e6ab34c639232da6afca393ebb6487ea2cc
Temerold <tobbe@temerold.se>
2022-06-04 22:28:35 +0200

    Rename count.exe and compile_count_c.bat

--- compile.bat
+++ compile.bat
@@ -1,4 +1,4 @@
 :: ! IMPORTANT: Remember to run the this file when `count.c` is altered -- to compile it
 :: ! -- and then add the .exe (c-count.exe) file to the git commit
 
-gcc count.c -o count
+gcc count.c -o C-Count
