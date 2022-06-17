f229bcacfc7f43538e330be6cc57b0703f9922f5
Temerold <tobbe@temerold.se>
2022-06-17 20:12:29 +0200

    Create `pipreqs_additional_libs.py`

---
+++ src/pipreqs_additional_libs.py
@@ -0,0 +1,6 @@
+# In this file, import every library which should be included in the requirement files
+# (which hasn't been imported elsewhere). Why? Because -- for some reason, pipreqs doesn't
+# have an option to add additional libraries which aren't used anywhere, but still are
+# needed in the project.
+
+import pipreqs # pipreqs itself
