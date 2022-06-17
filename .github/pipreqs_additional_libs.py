8d662582c202837893a240177031e6c97bc10535
Temerold <tobbe@temerold.se>
2022-06-17 20:12:41 +0200

    Delete `pipreqs_additional_libs.py`

--- .github/pipreqs_additional_libs.py
+++
@@ -1,6 +0,0 @@
-# In this file, import every library which should be included in the requirement files
-# (which hasn't been imported elsewhere). Why? Because -- for some reason, pipreqs doesn't
-# have an option to add additional libraries which aren't used anywhere, but still are
-# needed in the project.
-
-import pipreqs # pipreqs itself
